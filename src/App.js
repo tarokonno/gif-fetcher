15ef00626eddc28a806f2b9d6fb42bd3acb0e991
tarokonno <tarokonno7@gmail.com>
2018-01-31 20:29:58 +1100

    update api to https

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default class App extends Component {
   }
 
   performSearch = (query = 'cats') => {
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+    axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
       .then(response => {
         this.setState({
           gifs: response.data.data,
@@ -31,8 +31,7 @@ export default class App extends Component {
       });
   }
 
-  render() { 
-    console.log(this.state.gifs);
+  render() {
     return (
       <div>
         <div className="main-header">
